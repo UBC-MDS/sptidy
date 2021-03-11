c64f08e2721f9cfb09aea1f97ee4f6dfa21efb36
jacobmcf <jacobmcf@student.ubc.ca>
2021-03-11 07:23:20 -0800

    Move test data to the right folder

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,12 +1,6 @@
 library(testthat)
 library(sptidy)
-library(tidyverse)
 
 test_check("sptidy")
 
 
-kmeans_helper <- function(){
-  kmeans_test_tib <- tibble('x' = c(1,1,1,1,3,3,3,3),
-                         'y' = c(1,3,7,9,1,3,7,9))
-  return(kmeans_test_tib)
-}
--- tests/testthat/test-sptidy.R
+++ tests/testthat/test-sptidy.R
@@ -1,3 +1,9 @@
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
+
+kmeans_helper <- function(){
+  kmeans_test_tib <- tibble('x' = c(1,1,1,1,3,3,3,3),
+                            'y' = c(1,3,7,9,1,3,7,9))
+  return(kmeans_test_tib)
+}
