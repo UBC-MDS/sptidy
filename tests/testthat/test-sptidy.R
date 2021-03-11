d5282f69ab10191860b9f11b7fa0f218e2453ab3
jacobmcf <jacobmcf@student.ubc.ca>
2021-03-11 14:19:18 -0800

    Fix issues with parentheses in kmeans

--- tests/testthat/test-sptidy.R
+++ tests/testthat/test-sptidy.R
@@ -2,8 +2,8 @@
 
 test_that("tidy_kmeans works as expected and fails gracefully", {
   # Fitting a kmeans model on our helper data, comparing it to our hand-calculated output
-  expect_true(dplyr::all_equal(sptidy::tidy_kmeans(stats::kmeans(helper_kmeans_data(),
-                                                                 centers = 2, helper_kmeans_data())),
+  expect_true(dplyr::all_equal(
+    sptidy::tidy_kmeans(stats::kmeans(helper_kmeans_data(), centers = 2), helper_kmeans_data()),
                                helper_kmeans_tidy_out()))
   # Checking that we fail gracefully when given a different class model
   expect_error(sptidy::tidy_kmeans(stats::lm(x ~ y, helper_kmeans_data())))
