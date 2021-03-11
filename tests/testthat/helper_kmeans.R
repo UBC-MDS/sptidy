7892f510967fbd60c1cb6784647237dcb4f7d144
jacobmcf <jacobmcf@student.ubc.ca>
2021-03-11 14:21:36 -0800

    Fix indexing bug in test data

--- tests/testthat/helper_kmeans.R
+++ tests/testthat/helper_kmeans.R
@@ -9,7 +9,7 @@ helper_kmeans_data <- function(){
 helper_kmeans_tidy_out <- function(){
   center_0 = tibble::tibble('x' = 2, 'y' = 2)
   center_1 = tibble::tibble('x' = 2, 'y' = 8)
-  kmeans_tidy_result_tib <- tibble::tibble("cluster_number" = c(0,1),
+  kmeans_tidy_result_tib <- tibble::tibble("cluster_number" = c(1,2),
                                    "cluster_center" = list(center_0, center_1),
                                    "n_points" = c(4,4))
   return(kmeans_tidy_result_tib)
