a6f0cdcfa72e359245b585c3571586579e05d054
heidi-ye <70611714+heidi-ye@users.noreply.github.com>
2021-03-11 16:35:10 -0800

    Adding augment_lr function

--- R/sptidy.R
+++ R/sptidy.R
@@ -1,3 +1,4 @@
+library(tidyverse)
 # test fcn
 
 #' Tidy Output for tidymodels Linear Regression
@@ -45,13 +46,13 @@ tidy_kmeans <- function(X, Model) {
 
 }
 
-#' Augmented Output for Tidymodel's Linear Regression
+#' Augmented Output for Base R's Linear Regression
 #'
-#' Adds two columns to the original data of the scikit learn's linear regression model. This includes predictions and residuals.
+#' Adds two columns to the original data of base R's linear regression model. This includes predictions and residuals.
 #'
 #' @params model _lm
 #' @params X data.frame
-#' 
+#' @params y data.frame
 #'
 #' @return output data.frame
 #' @export
@@ -59,16 +60,18 @@ tidy_kmeans <- function(X, Model) {
 #' @examples
 #' # Import libraries
 #' library(tidyverse)
-#' library(tidymodels)
-#' # Load data
 #' data("longley")
-#' # Fit linear regression
-#' my_lr <- linear_reg() %>%
-#' set_engine("lm") %>%
-#' fit(Employed~., data = longley)
-#' # Return augmented dataframe
+#' my_lr <- lm(Employed~., data = longley)
 #' augment_lr(my_lr,longley)
-augment_lr <- function() {
+augment_lr <- function(my_lr, X, y) {
+  pred <- data.frame(predictions = predict(my_lr, X))
+
+  resid <- data.frame(y - pred) %>%
+    rename(residuals = Employed)
+
+  output <- cbind(X, y, pred, resid)
+
+  return(output)
 }
 
 #' Cluster Assignments of the Original Data Set
